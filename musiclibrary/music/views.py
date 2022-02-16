d77d357005f5fceebfd5c51ec2c6a5b8d1cb6dd0
dstoffels <dan.stoffels@gmail.com>
2022-02-16 12:01:00 -0600

    added SongDetail view, registered url

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -19,14 +19,3 @@ class SongList(APIView):
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class SongDetail(APIView):
-  def get_song(self, id):
-    try:
-      return Song.objects.get(pk=id)
-    except Song.DoesNotExist:
-      raise Http404
-
-  def get(self, request, id):
-    song = self.get_song(id)
-    serializer = SongSerializer(song)
-    return Response(serializer.data)
