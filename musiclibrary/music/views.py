07d9fd3a63226feefbeaab79dc5adf87e770615f
dstoffels <dan.stoffels@gmail.com>
2022-02-16 12:29:01 -0600

    added put and delete methods to SongDetail view

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -19,3 +19,27 @@ class SongList(APIView):
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class SongDetail(APIView):
+  def get_song(self, id):
+    try:
+      return Song.objects.get(pk=id)
+    except Song.DoesNotExist:
+      raise Http404
+
+  def get(self, request, id):
+    song = self.get_song(id)
+    serializer = SongSerializer(song)
+    return Response(serializer.data)
+
+  def put(self, request, id):
+    song = self.get_song(id)
+    serializer = SongSerializer(song, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, id):
+    song = self.get_song(id)
+    song.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
